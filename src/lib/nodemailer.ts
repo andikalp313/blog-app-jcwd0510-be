ba44a0c8e54b6e9a8877002576b691c0ee69efca
andikalp313 <andikaluhurpambudi@gmail.com>
2024-12-09 15:36:47 +0700

    feat: forgot password

---
+++ src/lib/nodemailer.ts
@@ -0,0 +1,9 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "GMAIL_EMAIL",
+    pass: "GMAIL_APP_PASSWORD",
+  },
+});
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import {
+  ForgotPasswordController,
   LoginController,
   registerController,
 } from "../controller/auth.controller";
-import { validateLogin, validateRegister } from "../validators/auth.validator";
+import {
+  validateForgotPassword,
+  validateLogin,
+  validateRegister,
+} from "../validators/auth.validator";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
 router.post("/login", validateLogin, LoginController);
+router.post(
+  "/forgot-password",
+  validateForgotPassword,
+  ForgotPasswordController
+);
 
 export default router;
