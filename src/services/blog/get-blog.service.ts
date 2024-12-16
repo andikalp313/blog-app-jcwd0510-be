4ee3b02029c7553f11d1d8ac7efc3aecd267a780
andikalp313 <andikaluhurpambudi@gmail.com>
2024-12-16 13:43:22 +0700

    feat: add blog detail

---
+++ src/services/blog/get-blog.service.ts
@@ -0,0 +1,16 @@
+import prisma from "../../lib/prisma";
+
+export const getBlogService = async (id: number) => {
+  try {
+    const blog = await prisma.blog.findUnique({
+      where: { id },
+      include: { user: { select: { name: true } } },
+    });
+    if (!blog) {
+      throw new Error("Invalid blog id");
+    }
+    return blog;
+  } catch (error) {
+    throw error;
+  }
+};
